0492bf284628b5238a9a9b40bda6ecd0f2309e62
Svitlana Lesiv <lesivsvitlana@gmail.com>
2020-03-16 21:28:24 -0700

    feat: show api response in a view

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,7 +1,15 @@
 function handleSubmit(event) {
   event.preventDefault();
 
-  const formUrl = document.getElementById("url").value;
+  const formUrlElement = document.getElementById("url").value;
+  const polarityElement = document.getElementById("polarity");
+  const subjectivityElement = document.getElementById("subjectivity");
+  const polarityConfidenceElement = document.getElementById("polarity_confidence");
+  const subjectivityConfidenceElement = document.getElementById(
+    "subjectivity_confidence"
+  );
+  const textBlockquoteElement = document.querySelector("#text > blockquote");
+
   // TODO: url validation
 
   console.log("::: Form Submitted :::");
@@ -11,10 +19,25 @@ function handleSubmit(event) {
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({ url: formUrl })
+    body: JSON.stringify({ url: formUrlElement })
   })
     .then(res => res.json())
-    .then(res => console.log(res));
+    .then(res => {
+      const {
+        polarity,
+        subjectivity,
+        polarity_confidence,
+        subjectivity_confidence,
+        text
+      } = res;
+      
+      polarityElement.innerHTML += ` ${polarity}`;
+      subjectivityElement.innerHTML += ` ${subjectivity}`;
+      polarityConfidenceElement.innerHTML += ` ${polarity_confidence}`;
+      subjectivityConfidenceElement.innerHTML += ` ${subjectivity_confidence}`;
+      textBlockquoteElement.innerHTML = text;
+      textBlockquoteElement.setAttribute('cite', formUrlElement);
+    });
 }
 
 export { handleSubmit };
