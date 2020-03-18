8b9e9b619b59d941fca53ee4a860f1791f6c4932
Svitlana Lesiv <lesivsvitlana@gmail.com>
2020-03-17 20:21:54 -0700

    feat: add loading

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,14 +1,20 @@
 function handleSubmit(event) {
   event.preventDefault();
 
-  const formUrlElement = document.getElementById("url").value;
+  const formUrlElementValue = document.getElementById("url").value;
   const polarityElement = document.getElementById("polarity");
   const subjectivityElement = document.getElementById("subjectivity");
-  const polarityConfidenceElement = document.getElementById("polarity_confidence");
+  const polarityConfidenceElement = document.getElementById(
+    "polarity_confidence"
+  );
   const subjectivityConfidenceElement = document.getElementById(
     "subjectivity_confidence"
   );
-  const textBlockquoteElement = document.querySelector("#text > blockquote");
+  const textElement = document.getElementById("text");
+  const loadingElement = document.getElementById("loading");
+  const resultsElement = document.getElementById("results");
+
+  loadingElement.innerHTML = "...Loading";
 
   // TODO: url validation
 
@@ -19,7 +25,7 @@ function handleSubmit(event) {
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({ url: formUrlElement })
+    body: JSON.stringify({ url: formUrlElementValue })
   })
     .then(res => res.json())
     .then(res => {
@@ -30,13 +36,16 @@ function handleSubmit(event) {
         subjectivity_confidence,
         text
       } = res;
-      
-      polarityElement.innerHTML += ` ${polarity}`;
-      subjectivityElement.innerHTML += ` ${subjectivity}`;
-      polarityConfidenceElement.innerHTML += ` ${polarity_confidence}`;
-      subjectivityConfidenceElement.innerHTML += ` ${subjectivity_confidence}`;
-      textBlockquoteElement.innerHTML = text;
-      textBlockquoteElement.setAttribute('cite', formUrlElement);
+
+      loadingElement.style.display = 'none';
+      resultsElement.style.display = 'block';
+
+      polarityElement.innerHTML = polarity;
+      subjectivityElement.innerHTML = subjectivity;
+      polarityConfidenceElement.innerHTML = polarity_confidence;
+      subjectivityConfidenceElement.innerHTML = subjectivity_confidence;
+      textElement.innerHTML = text;
+      textElement.setAttribute("cite", formUrlElement);
     });
 }
 
