30b09a81f28f8d903819fce8f1cb5f0d4e2b257e
Svitlana Lesiv <lesivsvitlana@gmail.com>
2020-03-22 21:56:14 -0700

    style: follow project instructions

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,13 @@
 import { handleSubmit } from "./js/formHandler";
 import { validateURL } from "./js/validateURL";
+
+import "./styles/resets.scss";
 import "./styles/base.scss";
+import "./styles/main.scss";
+import "./styles/header.scss";
+import "./styles/form.scss";
+import "./styles/button.scss";
+import "./styles/footer.scss";
+import "./styles/output.scss";
 
 export { handleSubmit, validateURL };
